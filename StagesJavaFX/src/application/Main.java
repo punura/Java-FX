33e8c0f0088c60cf088b365b8e9b94c884924cdf
Punura Wathsara <123pemmawadupunura@gmail.com>
2022-10-24 22:28:36 +0530

    Added new file StagesJavaFX

---
+++ StagesJavaFX/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			Group root = new Group();
+			Scene scene = new Scene(root, 400, 400, Color.BLACK);
+			stage.setTitle("Title");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ StagesJavaFX/src/module-info.java
@@ -0,0 +1,6 @@
+module StagesJavaFX {
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
